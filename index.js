5201521a820431014e7eac83259c733790395e3b
Sarah Dean <sarah.schmidtdean@gmail.com>
2023-03-30 23:15:50 -0500

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+// Want array with this info:
+   // name: Sam;
+    //Address: 11 Broadway;
+//
+const employee = {
+    name: "Sam",
+    address: "11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employees = {... employee};
+employees[key] = value;
+return employees;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employees = {... employee};
+    delete employees[key];
+    return employees;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
